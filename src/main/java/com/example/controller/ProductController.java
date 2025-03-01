eb26febe2923c0f2f7db5c4b2df60a6e5304d1bf
Elkott1 <103424889+Elkott1@users.noreply.github.com>
2025-03-01 19:10:30 +0200

    Finished product controller

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -1,6 +1,10 @@
 package com.example.controller;
 
 import com.example.model.Product;
+import com.example.service.CartService;
+import com.example.service.ProductService;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,37 +14,46 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @PostMapping("/")
     public Product addProduct(@RequestBody Product product) {
-        return null;
+        return productService.addProduct(product);
     }
 
     @GetMapping("/")
     public ArrayList<Product> getProducts() {
-        return null;
+        return productService.getProducts();
     }
 
     @GetMapping("/{productId}")
     public Product getProductById(@PathVariable UUID productId) {
-        return null;
+        return productService.getProductById(productId);
     }
 
     @PutMapping("/update/{productId}")
-    public Product updateProduct(@PathVariable UUID productId, @RequestBody Map<String,Object>
-            body) {
-        return null;
+    public Product updateProduct(@PathVariable UUID productId, @RequestBody Map<String, Object> body) {
+
+        String newName = (String) body.get("newName");
+        double newPrice = Double.parseDouble(body.get("newPrice").toString());
+        return productService.updateProduct(productId, newName, newPrice);
     }
 
     @PutMapping("/applyDiscount")
-    public String applyDiscount(@RequestParam double discount,@RequestBody ArrayList<UUID>
-            productIds) {
-        return null;
+    public String applyDiscount(@RequestParam double discount, @RequestBody ArrayList<UUID> productIds) {
+        productService.applyDiscount(discount, productIds);
+        return "Discount applied successfully!";
     }
 
     @DeleteMapping("/delete/{productId}")
     public String deleteProductById(@PathVariable UUID productId) {
-        return null;
+        productService.deleteProductById(productId);
+        return "Product deleted successfully!";
     }
 
 }
